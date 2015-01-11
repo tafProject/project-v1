fa5379ab5c0e4caf9010c9d36dbdc7e48591da8d
Zenzamy Othmane <ozenzamy@gmail.com>
2015-01-11 18:09:23 +0000

    Modification des parametre de la base de donnee

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'project_v-1',
+			'username'  => 'root',
+			'password'  => 'Othmane1',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
